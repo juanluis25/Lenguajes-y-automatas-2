2f53bf88ed5741c87bf724e089f4d98be178d409
R3YNA <43867521+R3YNA@users.noreply.github.com>
2018-10-15 14:25:42 -0600

    Update Compilador.java

--- src/proyecto1/Compilador.java
+++ src/proyecto1/Compilador.java
@@ -1,6 +1,5 @@
- package proyecto1;
+package proyecto1;
 import java.awt.Color;
-
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,22 +17,22 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 public class Compilador extends JFrame {
-	//Declaraci�n de variables
+	//Declaración de variables
 	File abre;
 	ArbolRecur arbol;
 	private static final long serialVersionUID = 1L;
-	JLabel label=new JLabel("Load a file or type code");
+	JLabel label=new JLabel("Cargar archivo");
 	JTextArea t=new JTextArea();
 	JScrollPane scroll;
-	JButton b2=new JButton ("Compile");
-	JButton b1=new JButton ("Load file");
-	JButton b3=new JButton ("Exit");
+	JButton b2=new JButton ("Compilar");
+	JButton b1=new JButton ("Cargar archivo");
+	JButton b3=new JButton ("Salir");
 	Oy1 o=new Oy1();
 	public static Compilador c;
 
 	public Compilador() {
 		setResizable(false);
-		setTitle("Compiler ");
+		setTitle("Compilador");
 		setVisible(true);
 		setSize(600,500);
 		JPanel j=new JPanel(); 
@@ -78,44 +77,44 @@ public class Compilador extends JFrame {
 	final JFileChooser fc = new JFileChooser();
 	public class Oy1 implements ActionListener	{
 		public void actionPerformed( ActionEvent e ){
-			//Si "GUARDAR" es el command que le pasamos es porque se habr� pulsado el bot�n nuevo_bt 
-			if ("Compile".equals(e.getActionCommand())){//				label.setBounds(new Rectangle(200,50,400,21));
+			//Si "GUARDAR" es el command que le pasamos es porque se habrá pulsado el botón nuevo_bt 
+			if ("Compilar".equals(e.getActionCommand())){//				label.setBounds(new Rectangle(200,50,400,21));
 				//System.out.println("compilado\n||-----\n"+t.getText()+"-----||");				//-------------------------Modificar
 				if(t.getText().equals("")){
-					c.setTitle("Empty text box");
-					JOptionPane.showMessageDialog(c, "You got no Strings on the text field!!!","Are you crazy mf? this shit empty",JOptionPane.ERROR_MESSAGE);
+					c.setTitle("Caja de texto vacía");
+					JOptionPane.showMessageDialog(c, "No hay cadenas en este archivo!!!","Está vacío",JOptionPane.ERROR_MESSAGE);
 				}else{
 					try {
 						//Parser p=new Parser(t.getText());
 						Pars p=new Pars(t.getText());
 						if (p.daval()==true) {
-							JOptionPane.showMessageDialog(c, "\tsuccessfully compiled \n"
-									+ "\tthe results are in the next file\nC:/Users/BadMf/Desktop/Resultados.java","Done",JOptionPane.INFORMATION_MESSAGE);
+							JOptionPane.showMessageDialog(c, "\tCompilación exitosa \n"
+									+ "\tLos resultados estan en el siguiente archivo\nC:/Users/BadMf/Desktop/Resultados.java","Done",JOptionPane.INFORMATION_MESSAGE);
 							//label.setText(c.getTitle());
-			                @SuppressWarnings("resource")
+			                @SuppressWarnings("recursos")
 							PrintStream DDescritor = new PrintStream("C:/Users/BadMf/Desktop/Resultados.java");
 			                DDescritor.println(t.getText());
-			                c.setTitle("compiled file "+abre.getName());
+			                c.setTitle("Archivo compilado "+abre.getName());
 						}else {
-							if(p.mensaje.equals("Expected boolean expression")){
-								JOptionPane.showMessageDialog(c, "Critical error on token: "+p.error+"\nType: " +p.getMensaje(),"Failure",JOptionPane.ERROR_MESSAGE);
-							}else if(p.mensaje.equals("Not the same type of variable")){
-								JOptionPane.showMessageDialog(c, "Critical error on tokens: "+p.auxiliar+", " +p.auxiliar2+"\nType: " +p.getMensaje(),"Failure",JOptionPane.ERROR_MESSAGE);
+							if(p.mensaje.equals("Se esperaba un boleano")){
+								JOptionPane.showMessageDialog(c, "Error de token: "+p.error+"\nTipo: " +p.getMensaje(),"Falla",JOptionPane.ERROR_MESSAGE);
+							}else if(p.mensaje.equals("No es el mismo tipo de variable")){
+								JOptionPane.showMessageDialog(c, "Error de tokens: "+p.auxiliar+", " +p.auxiliar2+"\nTipo: " +p.getMensaje(),"Failure",JOptionPane.ERROR_MESSAGE);
 								}else{
-							JOptionPane.showMessageDialog(c, "Critical error on token: "+p.getTok()+"\nType: " +p.getMensaje(),"Failure",JOptionPane.ERROR_MESSAGE);
+							JOptionPane.showMessageDialog(c, "Error de tokens: "+p.getTok()+"\nTipo: " +p.getMensaje(),"Falla",JOptionPane.ERROR_MESSAGE);
 							}
-							c.setTitle("I can't proccess this file "+abre.getName());
+							c.setTitle("No se puede abrir este archivo "+abre.getName());
 						}
 					} catch (Exception e2) {			 
 					}			
 				}
 			} 
-			if ("Load file".equals(e.getActionCommand())){
+			if ("Cargar ARCHIVO".equals(e.getActionCommand())){
 				abrirArchivo();
 				//System.out.println("Load file");
 				
 			} 
-			if ("Exit".equals(e.getActionCommand())){
+			if ("Salir".equals(e.getActionCommand())){
 				//System.out.println("Exit");
 				System.exit(0);
 			} 
@@ -131,7 +130,7 @@ public class Compilador extends JFrame {
 			 file.showOpenDialog(this);
 			 /**abrimos el archivo seleccionado*/
 			 abre=file.getSelectedFile();
-			 c.setTitle("Trying to compile "+abre.getName());
+			 c.setTitle("Intentando compilar "+abre.getName());
 			 /**recorremos el archivo, lo leemos para plasmarlo
 			 *en el area de texto*/
 			 
@@ -145,18 +144,18 @@ public class Compilador extends JFrame {
 			     lee.close();
 			 }    
 		 } catch(Exception ex){
-		     JOptionPane.showMessageDialog(c,"MF there's no fucking file you dumbass",
+		     JOptionPane.showMessageDialog(c,"No hay nada",
 		     "Warning!!!",JOptionPane.WARNING_MESSAGE);
 		 }
 		 return texto;//El texto se almacena en el JTextArea
 	}
 	public static void main(String[] args) {
 		try {
-			c =new Compilador();
+			c=new Compilador();
 		} catch (Exception e) {
 			  JOptionPane.showMessageDialog(null,e+"" +
 					     "\nError",
-					     "Check your code!!!",JOptionPane.WARNING_MESSAGE);
+					     "Revisa el código!!!",JOptionPane.WARNING_MESSAGE);
 			
 		}
 		
